f3279a99a88270ec30fa9e0be4ede46bade4a0c2
demoGit <ngoduchungnaruto@gmail.com>
2019-08-30 15:36:35 +0700

    fix css

--- src/components/home/Detail.js
+++ src/components/home/Detail.js
@@ -1,85 +1,54 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
-import facephoto from "./1.jpg";
-import Avatar from "@material-ui/core/Avatar";
-import Box from "@material-ui/core/Box";
 import { useQuery } from '@apollo/react-hooks';
-import { CHECK_INS, USER } from "../../documents/query";
+import { USER } from "../../documents/query";
 import { Table } from 'antd';
 import './../../styles/home/detail.css';
 
 const Detail = () => {
-    // const { loading, error, data } = useQuery(CHECK_INS);    
-    const { loading, error, data } = useQuery( USER, {variables :{ id:'5d655a7583829e0b240a0afb' } });
-    // if (loading) return 'Loading...';
-    // if (error) return `Error! ${error.message}`;
+    const { loading, error, data } = useQuery( USER, {variables :{ id:'5d68da3fc8785b0e70a8cc66' } });
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
     console.log(data);
-    const checkIn = [];
-	checkIn.push(data.user.checkIns);
 	return (
-		<Card>
-            <Box className = 'box'> 
-                        <Avatar
-                            style = {{width:'200px', height:'200px'}}
-                            alt="facephoto"
-                            src={facephoto}
-                            className='avatar'
-                        />
-                <Box style={{display:'flex',flexWrap:'wrap'}}>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block" p={1} bgcolor="background.paper">
-							<Box className='label'>Họ và tên: </Box>
-                    </Box>
-                    <Box component="span" display="block" p={1} bgcolor="background.paper">
-							<Box>{data.user.name}</Box>
-					</Box>
-                </Grid>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block" p={1} bgcolor="background.paper">
-							<Box className='label'>Tuổi: </Box>
-					</Box>
-                    <Box component="span" display="block"p={1} bgcolor="background.paper">
-                    {data.user.age}
-                    </Box>
-                </Grid>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block"p={1} bgcolor="background.paper">
-							<Box className='label'>Địa chỉ: </Box>
-                    </Box>
-                    <Box component="span" display="block"p={1} bgcolor="background.paper">
-                    {data.user.address}
-                    </Box>
-                </Grid>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block"p={1} bgcolor="background.paper">
-							<Box className='label'>Phụ Huynh : </Box>
-                    </Box>
-                    <Box component="span" display="block"p={1} bgcolor="background.paper">
-                    {data.user.status}
-                    </Box>
-                </Grid>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block"p={1} bgcolor="background.paper">
-							<Box className='label'>Điện Thoại: </Box>
-                    </Box>
-                    <Box component="span" display="block"p={1} bgcolor="background.paper">
-                    {data.user.Table}
-                    </Box>
-                </Grid>
-                <Grid style = {{width:"400px",height:'43px'}} container className='container'>
-                    <Box className='title' component="span" display="block" p={1} bgcolor="background.paper">
-							<Box className='label'>status:  </Box>
-                    </Box>
-                    <Box component="span" display="block" p={1} bgcolor="background.paper">
-							<Box>{data.user.status}</Box>
-					</Box>
-                </Grid>
-                </Box>
-            </Box>
+		<div>
+            <div className = 'box'>
+                <div style={{display:'flex',flexWrap:'wrap'}}>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span" bgcolor="background.paper">
+							<div className='label'>Họ và tên:  {data.user.name}</div>
+                    </div>
+                    
+                </div>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span" bgcolor="background.paper">
+							<div className='label'>Tuổi:  {data.user.age}</div>
+					</div>
+                    
+                </div>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span" bgcolor="background.paper">
+							<div className='label'>Địa chỉ:  {data.user.address}</div>
+                    </div>
+                    
+                </div>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span" bgcolor="background.paper">
+							<div className='label'>Phụ Huynh :  {data.user.status} </div>
+                    </div>
+                    
+                </div>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span" bgcolor="background.paper">
+							<div className='label'>Điện Thoại:  {data.user.Table}</div>
+                    </div>
+                </div>
+                <div style = {{width:"400px",height:'43px'}} container className='container'>
+                    <div className='title' component="span"  bgcolor="background.paper">
+							<div className='label'>status:  {data.user.status}  </div>
+                    </div>
+                </div>
+                </div>
+            </div>
             <Table
 				columns={[
 					{
@@ -95,8 +64,8 @@ const Detail = () => {
 						dataIndex: 'status',
 					}
 				]}
-				 dataSource={checkIn} />
-		</Card>
+				 dataSource={data.user.checkIns} />
+		</div>
 	);
 };
 
